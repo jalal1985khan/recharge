3f6f401f5ef1cd47fa27adccf0a5d8a202d2916b
jalal1985khan <jalal1985khan@gmail.com>
2022-12-19 10:14:51 +0530

    index changed

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if(isset($_POST['login'])){
 
 <body>
     <nav class="light-blue lighten-1" role="navigation">
-        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Recharge123</a>
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Recharge12340</a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="#">Navbar Link</a></li>
             </ul>
